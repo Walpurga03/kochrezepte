70da2b49f780737a28c26833ad9fbc41950a72a2
alex <alex.moik@gmx.at>
2024-10-21 16:55:24 +0200

    change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/kochrezepte/', // Setze den Basis-Pfad auf den Repository-Namen
   plugins: [react()],
-})
+});
